22fb38373a3d659a7183022d89cf4f9a19f33fc5
Thrishul Kumar Arraboina <48954717+trishulkumar@users.noreply.github.com>
2021-08-09 20:22:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask,render_template,request
+import pickle
+model=pickle.load(open("model.pkl","rb"))
+
+app=Flask(__name__)
+#jinga2 
+@app.route("/")
+def man():
+    
+    return render_template("Billboard.html")
+
+@app.route("/predict",methods=["POST"])
+
+def home():
+    # for rendering results on HTML GUI
+    data1=request.form["a"]
+    data2=request.form["b"]
+    data3=request.form["c"]
+    data4=request.form["d"]
+    data5=request.form["e"]
+    data6=request.form["f"]
+    data7=request.form["g"]
+    data8=request.form["h"]
+    data9=request.form["i"]
+    data10=request.form["j"]
+    
+    arr=[data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]
+    pred=model.predict([arr])
+    return render_template("after.html",data=pred)
+
+if __name__== "__main__":
+    app.run(debug=True)
+
